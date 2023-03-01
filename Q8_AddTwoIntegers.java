a1d778ab849a067be1546c0fa27d2cd9db1be732
Harshil Sharma <91001586+harshilshrma@users.noreply.github.com>
2023-03-01 23:02:11 +0530

    Add files via upload

---
+++ Q8_AddTwoIntegers.java
@@ -0,0 +1,10 @@
+// Date: 01 March 2023
+// Q8: Given two integers num1 and num2, return the sum of the two integers.
+
+package LeetCode;
+public class Q8_AddTwoIntegers {
+    public int sum(int num1, int num2) {
+        return num1+num2;
+    }
+}
+
